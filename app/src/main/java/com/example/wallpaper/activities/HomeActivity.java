3d5d8233065e88a715b62bd145bce38bd3ac535d
Akshat <mr.akshatagarwal26@gmail.com>
2019-06-18 14:29:10 +0530

    added home activity backend code

--- app/src/main/java/com/example/wallpaper/activities/HomeActivity.java
+++ app/src/main/java/com/example/wallpaper/activities/HomeActivity.java
@@ -1,15 +1,54 @@
 package com.example.wallpaper.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.wallpaper.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        displayFragment(new HomeFragment());
+    }
+
+    private void displayFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_area, fragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment;
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.nav_fav:
+                fragment = new FavouritesFragment();
+                break;
+            case R.id.nav_set:
+                fragment = new SettingsFragment();
+                break;
+            default:
+                fragment = new HomeFragment();
+                break;
+        }
+        displayFragment(fragment);
+        return true;
     }
 }
